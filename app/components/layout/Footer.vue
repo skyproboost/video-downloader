3aed601988ded0a86c9368b417d4a0a3dacbb9cc
skyproboost <skyproboost@mail.ru>
2026-02-18 17:36:46 +0300

    update

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -24,7 +24,7 @@
             <div class="footer__bottom">
                 <p class="footer__description">{{ $t('footer.description') }}</p>
                 <p class="footer__copy">
-                    © {{ currentYear }} aDownloader. {{ $t('footer.rights') }}
+                    © {{ currentYear }} VideoDownloader. {{ $t('footer.rights') }}
                 </p>
             </div>
         </div>
@@ -46,9 +46,8 @@ interface GroupedLinks {
 const localePath = useLocalePath()
 const currentYear = new Date().getFullYear()
 
-const { data: groupedLinks } = useFetch<GroupedLinks[]>('/api/footer-links', {
+const { data: groupedLinks } = await useFetch<GroupedLinks[]>('/api/footer-links', {
     key: 'footer-links',
-    server: false,
     default: () => [],
 })
 </script>
